f007db91babc286e8aba9f9572b776be4910f02c
ISol <599304776@qq.com>
2020-08-05 11:27:58 +0800

    deploy

---
+++ precache-manifest.a36340f8fc3f8c27113f1073ff811ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd0dd73e78831c068531e4eec71afb4",
+    "url": "/money-react-website/index.html"
+  },
+  {
+    "revision": "aae4626e29617147d044",
+    "url": "/money-react-website/static/css/main.8f4173d0.chunk.css"
+  },
+  {
+    "revision": "07271b00b594abcf7c29",
+    "url": "/money-react-website/static/js/2.8c4febdf.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-react-website/static/js/2.8c4febdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae4626e29617147d044",
+    "url": "/money-react-website/static/js/main.6b85475e.chunk.js"
+  },
+  {
+    "revision": "63972f710456e9074212",
+    "url": "/money-react-website/static/js/runtime-main.5f2a9baa.js"
+  }
+]);
